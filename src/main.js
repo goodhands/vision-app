cfcc694562aa2951aa480fb2b0348d3b2fd933c1
Samuel Olaegbe <olaegbesamuel@gmail.com>
2020-09-14 19:50:51 +0100

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from 'axios';
 
 Vue.config.productionTip = false
+
+axios.defaults.baseURL = 'https://api.example.com';
 
 new Vue({
   render: h => h(App),
