922eee02751a24638b88ec2e6987ead5292a09e6
Samuel Olaegbe <olaegbesamuel@gmail.com>
2020-09-15 21:56:06 +0100

    feat:handle file uploads and labelling

--- src/components/CloudinaryUpload.vue
+++ src/components/CloudinaryUpload.vue
@@ -2,7 +2,7 @@
     <div>
         <div class="flex h-64 items-center justify-center rounded-md w-full bg-gray-600">
             <div class="bg-opacity-50 bg-white flex flex-col h-32 items-center justify-center w-56">
-                <small class="text-gray-600" for="input">Insert an image URL</small>
+                <small class="text-gray-600" for="input">Click to begin upload</small>
                 <button type="button" @click="openMediaWidget" class="bg-gradient-to-b bg-gray-200 font-sans mb-5 mt-5 px-3 py-2 rounded shadow-2xl text-white hover:shadow-none">
                     Launch media upload
                 </button>
@@ -12,31 +12,49 @@
 </template>
 
 <script>
+import ImageLabel from '../services/imageLabel';
 export default {
     name: "CloudinaryUpload",
     data() {
         return {
-            url: '',
+            images: [],
+            label: new ImageLabel(),
+            result: [],
+            busy: false,
         }
     },
     methods: {
         openMediaWidget(){
-            window.cloudinary.openUploadWidget({ cloud_name: 'tmp',
-                upload_preset: 'vy3bve7h'
+            window.cloudinary.openUploadWidget(
+            { 
+                cloud_name: process.env.VUE_APP_CDNRY_NAME,
+                upload_preset: process.env.VUE_APP_CDNRY_PRESET
             },(error, result) => {
+                //save image urls on success
                 if (!error && result && result.event === "success") {
-                    console.log('Done uploading..: ', result.info);
-                    this.url = result.info.url;
+                    this.images.push(result.info.url);
+                }
+
+                if (!error && result && result.event === "close") {
+                    this.busy = true;
+                    this.labelImages();
                 }
             }).open();
-        }
-    },
+        },
 
-    mounted() {
-        //mount cloundinary
-        // setTimeout(() => {
-        //     this.mountWidget();
-        // }, 1200);
+        labelImages(){
+            this.label.buildRequest(this.images);
+
+            this.label.annotate()
+            .then(response => {
+                this.busy = false;
+                this.result = response.data;
+            })
+            .catch(error => {
+                this.busy = false;
+                console.log(error);
+            })
+        }
     },
 }
 </script>
