da5f74ac30f4832871df362a51ae858dc111901e
Samuel Olaegbe <olaegbesamuel@gmail.com>
2020-09-15 01:31:05 +0100

    add imageUploader component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
-  <div id="app" class="h-screen py-20">
-    <main class="content">
+  <div id="app" class="h-screen py-20 bg-gradient-to-r from-purple-400 to-red-500 via-pink-500">
+    <main class="m-auto max-w-screen-sm">
+      <image-uploader></image-uploader>
       <input type="text" class="border-2" v-model="url">
       <button @click="label">Get labels</button>
       <pre>
@@ -12,10 +13,12 @@
 
 <script>
 import ImageLabel from './services/imageLabel';
+import ImageUploader from './components/ImageUploader';
 
 export default {
   name: 'App',
   components: {
+    ImageUploader
   },
   data() {
     return {
