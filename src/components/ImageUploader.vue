62d453a246f3d86ccfc37ef73e249f363bedb7f3
Samuel Olaegbe <olaegbesamuel@gmail.com>
2020-09-15 08:23:59 +0100

    add loading state to button, support for multiple images

--- src/components/ImageUploader.vue
+++ src/components/ImageUploader.vue
@@ -6,14 +6,16 @@
                 <input type="text" v-if="!fileUpload" class="border-2 rounded-md" v-model="image" id="">
             </div>
         </div>
-        <button class="bg-gradient-to-b bg-gray-200 font-sans mb-5 mt-5 px-3 py-2 rounded shadow-2xl text-white" @click="fetchLabels">
+        <button :disabled="busy" class="bg-gradient-to-b bg-gray-200 font-sans mb-5 mt-5 px-3 py-2 rounded shadow-2xl text-white hover:shadow-none" @click="fetchLabels">
             Fetch labels
+            <span v-if="busy" title="Please wait" class="animate-ping bg-gray-200 h-3 inline-block inline-flex rounded-full w-3"></span>
         </button>
 
         <h2 class="text-3xl font-bold" v-if="!busy">
-            <span v-for="label in labels.responses" :key="label">
+            <span v-for="(label, parentIndex) in labels.responses" :key="label">
+                <h2>Image {{ parentIndex }}</h2>
                 <span v-for="(annotations, index) in label.labelAnnotations" :key="index" class="bg-gray-100 bg-opacity-50 font-light mx-2 mb-2 inline-block p-3 rounded text-base text-pink-600">
-                    {{ annotations.description}}
+                   {{ annotations.description}}
                 </span>
             </span>
         </h2>
