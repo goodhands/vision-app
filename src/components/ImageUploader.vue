3b09eadf7d1f02d726c94eb52ab2e22bb6d0e421
Samuel Olaegbe <olaegbesamuel@gmail.com>
2020-09-15 01:51:11 +0100

    handle file upload on frontend

--- src/components/ImageUploader.vue
+++ src/components/ImageUploader.vue
@@ -2,8 +2,8 @@
     <div>
         <div class="flex h-64 items-center justify-center rounded-md w-full bg-gray-600" :style="image.length > 0 && { background: `url(${image})` }">
             <div class="bg-opacity-50 bg-white flex flex-col h-32 items-center justify-center w-56">
-                <input type="file" class="flex overflow-hidden pb-4 text-sm w-10/12" name="" id="">
-                <input type="text" class="border-2 rounded-md" v-model="image" id="">
+                <input type="file" @change="handleImageUpload" accept="image/*" class="flex overflow-hidden pb-4 text-sm w-10/12" id="fileUpload">
+                <input type="text" v-if="!fileUpload" class="border-2 rounded-md" v-model="image" id="">
             </div>
         </div>
         <h2 class="text-3xl font-bold">
@@ -17,8 +17,27 @@ export default {
     data() {
         return {
             image: '',
-            labels: []
+            labels: [],
+            fileReader: new FileReader(),
+            fileUpload: false,
         }
+    },
+
+    methods: {
+        handleImageUpload(event){
+            this.fileUpload = true;
+
+            this.fileReader.onload = () => {
+                this.image = this.fileReader.result;
+            }
+
+            //set the raw file to avatar for form upload
+            // this.avatar = event.target.files[0];
+
+            //stream the file preview 
+            this.fileReader.readAsDataURL(event.target.files[0]);
+
+        },
     },
 }
 </script>
