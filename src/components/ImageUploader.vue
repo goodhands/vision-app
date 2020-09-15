a000c44e5cf61d64ea6fd05fc5c152773ee0f9d7
Samuel Olaegbe <olaegbesamuel@gmail.com>
2020-09-15 01:38:38 +0100

    customize the image uploader box

--- src/components/ImageUploader.vue
+++ src/components/ImageUploader.vue
@@ -1,9 +1,9 @@
 <template>
     <div>
-        <div class="flex h-64 items-center justify-center rounded-md w-full bg-gray-600">
+        <div class="flex h-64 items-center justify-center rounded-md w-full bg-gray-600" :style="image.length > 0 && { background: `url(${image})` }">
             <div class="bg-opacity-50 bg-white flex flex-col h-32 items-center justify-center w-56">
                 <input type="file" class="flex overflow-hidden pb-4 text-sm w-10/12" name="" id="">
-                <input type="text" class="border-2 rounded-md" name="" id="">
+                <input type="text" class="border-2 rounded-md" v-model="image" id="">
             </div>
         </div>
         <h2 class="text-3xl font-bold">
@@ -16,6 +16,7 @@
 export default {
     data() {
         return {
+            image: '',
             labels: []
         }
     },
