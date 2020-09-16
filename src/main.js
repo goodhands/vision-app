fac8acc12ed73b645172f5d8702a3d725ea18f06
Samuel Olaegbe <olaegbesamuel@gmail.com>
2020-09-16 20:30:16 +0100

    fix stylesheet problems

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import axios from 'axios'
 import './assets/tailwind.css'
+import './assets/flickity.css'
 import router from './router'
 
 axios.defaults.baseURL = "https://vision.googleapis.com/v1"
